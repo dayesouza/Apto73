a2b5e35f605ce5c267292bc7b50fb17fcb7f0c3c
Day Souza <day.carol18@gmail.com>
2021-04-17 20:21:03 -0300

    fix mongodb connection string

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+require('dotenv').config();
 
-const username = process.env.DB_NAME;
+const mongoose = require('mongoose');
+const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const container = process.env.CONTAINER;
-const databaseName = process.env.DATABASE_NAME;
+const databaseName = process.env.DB_NAME;
 
-let mongoUrl = `mongodb://${username}:${password}@${container}.0u4ah.azure.mongodb.net/${databaseName}?retryWrites=true&w=1`;
+let mongoUrl = `mongodb+srv://${username}:${password}@${container}.0u4ah.azure.mongodb.net/${databaseName}?retryWrites=true&w=1`;
 if (process.env.NODE_ENV.toLocaleLowerCase() === 'local') {
   mongoUrl = 'mongodb://127.0.0.1:27017/apto73';
 }
